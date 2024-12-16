a7d5b766643fdb3566fd41ab8bcce81ec742ee3e
adithyachandranv <adithyachandranv@gmail.com>
2024-12-16 20:22:09 +0530

    others intial

--- public/js/other.js
+++ public/js/other.js
@@ -3,11 +3,11 @@ function openTab(tabId) {
   const contents = document.querySelectorAll('.tab-content');
   contents.forEach(content => content.classList.remove('active'));
 
-  
+
   const buttons = document.querySelectorAll('.tab-button');
   buttons.forEach(button => button.classList.remove('active'));
 
-  
+
   document.getElementById(tabId).classList.add('active');
   const activeButton = Array.from(buttons).find(button => button.innerText === tabId.replace('tab', 'Tab '));
   activeButton.classList.add('active');
